8e3ebc691dfc81a9951c4bc7e0baca85758a2ba1
Lanz <lanzdagamer@gmail.com>
2022-08-19 16:47:26 -0400

    fixing duplicate nodes, bug

---
+++ src/utils/build-graph-local.js
@@ -0,0 +1,116 @@
+import parseCourseRelationships from './parse-course-rels.js';
+
+/**
+ *
+ * @param {string} rootCourse 
+ * @returns list of nodes and edges
+ */
+export async function buildGraph(rootCourse) {
+    /**
+     * Get the department of rootCourse
+     * Examples:
+     * 
+     * 'BMGT313F' -> 'BMGT'
+     * 'JOUR175' -> 'JOUR'
+     */
+    // const department = rootCourse.slice(0, 4);
+
+    /**
+     * build the url 
+     * 
+     * For local development, we will use mock data using json-server library.
+     * 
+     * Command to start server: 'json-server --watch data/cmscCourses.json -p 8000'
+     * Url will be opened to http://127.0.0.1:8000/courses
+     */
+    const url = 'http://127.0.0.1:8000/courses';
+
+    // build course prereq map
+    const coursePrereqMap = await buildCoursePrereqMap(url);
+    
+    let nodes = [];
+    let edges = [];
+
+    function dfs(prev, curr) {
+        if (curr) {
+            nodes.push(buildNode(curr));
+        }
+        
+        if (prev) {
+            edges.push(buildEdge(curr, prev));
+        }
+
+        let relationships = coursePrereqMap.get(curr);
+        if (!relationships) {
+            return;
+        }
+
+        // search mustTake path
+        for (let prereq of relationships.mustTake) {
+            // 'curr' passed in first param of dfs because that's where the next recursion call
+            // previously came from.
+            dfs(curr, prereq);
+        }
+
+        // search pickOneFromEach
+        for (let prereqCluster of relationships.pickOneFromEach) {
+            let nodeId = prereqCluster.join('or');
+            nodes.push(buildNode(nodeId));
+            edges.push(buildEdge(nodeId, curr));
+        }
+
+        // search pickTwoFromEach
+        let pickTwoPrereqs = relationships.pickTwoFromEach;
+        if (pickTwoPrereqs.length > 0) {
+            let nodeId = pickTwoPrereqs.join('two');
+            nodes.push(buildNode(nodeId));
+            edges.push(buildEdge(nodeId, curr));    
+        }
+    }
+
+    dfs(null, rootCourse); // pass first param as null because rootCourse has no parent node.
+
+    return [nodes, edges];
+}
+
+async function buildCoursePrereqMap(url) { // department param will not used for local development
+    let courses = [];
+    let coursePrereqMap = new Map();
+
+    try {
+        const res = await fetch(url);
+        const coursesInDepartment = await res.json();
+        courses.push(...coursesInDepartment);
+    } catch (error) {
+        console.log('could not fetch data');
+    }
+
+    courses.forEach(course => {
+        let prereqsObject = null;
+        const strOfPrereqs = course.relationships.prereqs;
+
+        if (strOfPrereqs) {
+            prereqsObject = parseCourseRelationships(strOfPrereqs);
+        }
+
+        // add course and its prerequisites to a map, for fast lookup
+        coursePrereqMap.set(course.course_id, prereqsObject);       
+    });
+
+    return coursePrereqMap;
+}
+
+function buildNode(courseId) {
+    return {
+        id: courseId,
+        text: courseId
+    }
+}
+
+function buildEdge(prereq, course) {
+    return {
+        id: `${prereq}-${course}`,
+        from: prereq,
+        to: course
+    }
+}
---
+++ src/utils/parse-course-rels.js
@@ -0,0 +1,100 @@
+//---------------------------------------------------------------------------//
+// Credits to konaraddi on GitHub for the parsePrereqs function and 
+// use of regex.
+//
+// https://github.com/konaraddi/umd-course-vis/blob/master/src/parsePrereqs.js
+//---------------------------------------------------------------------------//
+
+function parseCourseRelationships(str) {
+    // LATER change @param str to @param relationships
+    // @param relationships - object with prereqs and coreqs
+    // 
+    // implement this later
+    // const parsedPrereqs = parsePrereqs(relationships.prereqs);
+    // const parsedCoreqs = parseCoreqs(relationships.coreqs);
+    // 
+    // const relationships = {
+    //     prereqs: parsedPrereqs,
+    //     coreqs: parsedCoreqs
+    // }
+    //
+    // return relationships
+    
+    const parsedPrereqs = parsePrereqs(str);
+    return parsedPrereqs;
+}
+
+function parsePrereqs(str) {
+    let prereqs = {
+        // student must take these courses
+        mustTake: [],
+
+        // student must take ONE of several prerequisites
+        pickOneFromEach: [],
+
+        // student must take TWO of several prerequisites
+        pickTwoFromEach: []
+    };
+
+    /**
+     * There are strings passed in where it contains the term '2 courses from'. This conflicts with
+     * the regex statement below, str.includes("from"), since its made to populate
+     * pickOneFromSeveral. Furthermore, '2 courses from' must be removed from the string, that is 
+     * the remaining string. Furthermore, pickTwoFromEachParsed is checked first before doing the
+     * rest.
+     */
+    let [remaining, pickTwoFromEachParsed] = parsePickTwoFromEach(str);
+    str = remaining; // '2 courses from' is now removed, so treat remaining as the main str to parse
+    prereqs.pickTwoFromEach = pickTwoFromEachParsed;
+
+    prereqs.mustTake = str.match(/[A-Z]{4}[0-9]{3}[A-Z]?/g);
+  
+    if (str.includes("from")  || str.includes("or equivalent programming experience")) {
+      const regex = /from \(([A-Z|0-9|,| ]*)\)/g;
+      let pickOneFrom;
+  
+      while ((pickOneFrom = regex.exec(str)) !== null) {
+        prereqs.pickOneFromEach.push(pickOneFrom[1].split(", "));
+      }
+  
+      for (let i = 0; i < prereqs.pickOneFromEach.length; i++) {
+        prereqs.mustTake = prereqs.mustTake.filter(
+          course => !prereqs.pickOneFromEach[i].includes(course)
+        );
+      }
+    }
+  
+    if (prereqs.mustTake == null) {
+      prereqs.mustTake = [];
+    }
+
+    if (prereqs.pickTwoFromEach.length > 0) {
+      let temp = prereqs.mustTake;
+
+      prereqs.mustTake = [];
+      prereqs.pickOneFromEach = [];
+      prereqs.pickOneFromEach.push(temp);
+    }
+  
+    return prereqs;
+}
+
+function parsePickTwoFromEach(str) {
+  let remaining = str;
+  let pickTwoFromEach = [];
+
+  let start = str.indexOf('2 courses from');
+  if (start != -1) {
+      let end = str.indexOf(';', start);
+
+      let strSnippet = str.slice(start + 16, end - 1);
+      strSnippet = strSnippet.replace('or ', '');
+
+      pickTwoFromEach = strSnippet.split(", ");
+      remaining = str.slice(0, start - 8) + str.slice(end - 1);
+  }
+
+  return [remaining, pickTwoFromEach];
+}
+
+export default parseCourseRelationships;
