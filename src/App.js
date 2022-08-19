92f04340be7e8f53cf2b5cd27bb51ae61d8185db
Lanz <lanzdagamer@gmail.com>
2022-08-19 16:45:57 -0400

    render PrereqTree in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import PrereqTree from "./PrereqTree";
+import { buildGraph } from './utils/build-graph-local.js';
 
 function App() {
+  // STATE
+  const [rootCourse, setRootCourse] = useState(null);
+  const [nodes, setNodes] = useState(null);
+  const [edges, setEdges] = useState(null);
+  const [isGraphBuilt, setIsGraphBuilt] = useState(false);
+
+  useEffect(() => {
+    async function fetchGraph() {
+      try {
+        let [nodesList, edgesList] = await buildGraph(rootCourse);
+  
+        setNodes(nodesList);
+        setEdges(edgesList);
+        setIsGraphBuilt(true);
+      } catch (error) {
+        console.log('error occurred');
+        console.log(error);
+      }
+    }
+
+    if (rootCourse) {
+      fetchGraph();
+    }  
+  }, [rootCourse]);
+
+
   return (
     <div className="container">
-      <h1>Testing</h1>
+      <input 
+        type="button"
+        value="Set Root Course"
+        onClick={() => {
+          console.log('BUTTON CLICKED');
+          setRootCourse('CMSC420');
+      }} />
+    { rootCourse && <h2> Prereq Tree for: { rootCourse } </h2>}
+    { isGraphBuilt && <PrereqTree nodes={nodes} edges={edges}/> }
     </div>
   );
 }
