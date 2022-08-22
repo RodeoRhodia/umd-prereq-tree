4ea9ec96f2ba3275249f7bace61be1d16480dd2d
RodeoRhodia <66293384+RodeoRhodia@users.noreply.github.com>
2022-08-21 22:13:06 -0400

    added value in custom hook

--- src/custom-hooks/useDepts.js
+++ src/custom-hooks/useDepts.js
@@ -6,13 +6,14 @@ const useDepts = (letter) => {
 
     const [departments, setDepartments] = useState(departmentsObj['startsWith' + letter]);
     const colNamesForDepts = ['Dept. Id', 'Department'];
+    const deptId = 'dept_id';
     
     useEffect(() => {
         let currentKey = 'startsWith' + letter; 
         setDepartments(departmentsObj[currentKey]);
     }, [letter]);
 
-    return { departments, colNamesForDepts };
+    return { departments, colNamesForDepts, deptId };
 }
 
 export default useDepts;
