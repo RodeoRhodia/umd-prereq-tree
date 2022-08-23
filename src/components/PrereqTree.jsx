5b3bfdf52989206ed907b49ca09d9ff70ae3691b
Lanz <lanzdagamer@gmail.com>
2022-08-23 14:07:27 -0400

    fetch and display more details of node

--- src/components/PrereqTree.jsx
+++ src/components/PrereqTree.jsx
@@ -4,7 +4,7 @@ const PrereqTree = ({ nodes, edges }) => {
     return (
       <Canvas
         fit={true}
-        maxWidth={800}
+        maxWidth={600}
         maxHeight={800}
         nodes={nodes}
         edges={edges}
@@ -14,7 +14,34 @@ const PrereqTree = ({ nodes, edges }) => {
             {...node}
             dragCursor="grab"
             dragType="all"
-            // onEnter={(event, node) => console.log(node)}
+            onClick={(event, node) => {
+              let textOfClickedNode = node.text;
+
+              async function displayMoreDetails() {
+                try {
+                  let url = `https://api.umd.io/v1/courses/${textOfClickedNode}`;
+
+                  let res = await fetch(url);
+                  let courseDetails = await res.json();
+                  let message = `
+                    Course Id: ${textOfClickedNode}, 
+                    Course: ${courseDetails[0].name}, 
+                    Department: ${courseDetails[0].department},
+                    Description: ${courseDetails[0].description.slice(0, 50)}...`;
+                  
+                  window.alert(message);
+                } catch (error) {
+                  console.log('could not get more details');
+                }
+              }
+
+              if (textOfClickedNode.slice(0, 2) !== 'P1' && textOfClickedNode.slice(0, 2) !== 'P2') {
+                displayMoreDetails();
+              } else {
+                window.alert('Cannot get details at this moment');
+              }
+            
+            }}
           />
         )}
       />
